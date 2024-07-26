714541c424661eb91b3c5d666a22375718e87d7b
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-26 20:03:28 +0530

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -1,1 +1,39 @@
+class generator;
+  
+  transaction tr;
+  
+  mailbox #(transaction) mbxgd;
+  
+  mailbox #(bit [4:0]) mbxgm;
+  
+  
+  event done; 
+  event drvnext; 
+  event sconext; 
+ 
+   int count = 0;
+   
+  
+  function new( mailbox #(transaction) mbxgd, mailbox #(bit[4:0]) mbxgm);
+    this.mbxgd = mbxgd; 
+    this.mbxgm = mbxgm;  
+    tr =new();
+  endfunction
+  
+  
+  
+    task run();
+    
+   repeat(count) begin
+      assert(tr.randomize) else $error("Randomization Failed"); 
+      $display("[GEN] : DATA SENT TO DRV");
+      mbxgd.put(tr.copy);
+      mbxgm.put(tr.ulen);
+      @(drvnext);
+      @(sconext);
+    end
+    
+    ->done;
+  endtask
 
+endclass
