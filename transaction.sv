6f5111748ebc0d2e90f3287d176b37e7e1fff4d1
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-26 20:04:00 +0530

    Update transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -1,1 +1,46 @@
-
+class transaction;
+  
+  rand bit [4:0] ulen;
+  rand bit [31:0] hwdata;
+  rand bit [31:0] haddr;
+  rand bit [2:0]  hsize; 
+  rand bit [2:0]  hburst;
+  bit hresetn;
+  rand bit hwrite;
+  bit [1:0] htrans;
+  bit [1:0] hresp;
+  bit hready;
+  bit [31:0] hrdata;
+  
+  constraint write_c {
+    hwrite dist {1 :/ 1, 0:/ 1};
+  }
+  
+  constraint ulen_c {
+    ulen == 5;
+  }
+  
+  
+  constraint burst_c {
+  hburst == 6;
+  }
+  
+  constraint addr_c {
+  haddr == 5;
+  }
+  
+  function transaction copy();
+    copy = new();
+    copy.hwdata = this.hwdata;
+    copy.haddr  = this.haddr;
+    copy.hsize  = this.hsize;
+    copy.hburst = this.hburst;
+    copy.hwrite = this.hwrite;
+    copy.htrans = this.htrans;
+    copy.hresp  = this.hresp;
+    copy.hready = this.hready;
+    copy.hrdata = this.hrdata;
+    copy.ulen   = this.ulen;
+  endfunction
+   
+ endclass
